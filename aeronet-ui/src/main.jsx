b35941db35cb7061ba655e4ba00034c1154b73d6
talatirutej <talatirutej@gmail.com>
2026-05-18 02:34:31 +0000

    Fix CSS import path after rename

--- aeronet-ui/src/main.jsx
+++ aeronet-ui/src/main.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './statcontour.css'
+import './aeronet.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
